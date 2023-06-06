5699eef5ad6f0237aafdf3a01c3d5c17a2535b35
FrancisDavison <francis.nj.davison@gmail.com>
2023-06-06 13:17:09 +0100

    made some progress, will finish later as lunch is over

--- OHM_Telemetry.R
+++ OHM_Telemetry.R
@@ -1,2 +1,6 @@
-Telemetry = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv', header=TRUE,skip=1)
+Telemetry_RAW = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv', header=T)
+Telemetry_Head = Telemetry_Raw[]
+Telemetry_Body = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv',skip=2)
+Telemetry_Plot = rbind(Telemetry_Head,Telemetry_Body)
+
 plot(Telemetry$CPU.Total, Telemetry$CPU.Package)
