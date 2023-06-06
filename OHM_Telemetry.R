7eae49d63e869242d2040c0d590d93f2dd15d5f2
FrancisDavison <francis.nj.davison@gmail.com>
2023-06-06 17:21:03 +0100

    got CSV imported to Object. Now to just build the plots

--- OHM_Telemetry.R
+++ OHM_Telemetry.R
@@ -1,6 +1,10 @@
-Telemetry_RAW = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv', header=T)
-Telemetry_Head = Telemetry_Raw[]
-Telemetry_Body = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv',skip=2)
-Telemetry_Plot = rbind(Telemetry_Head,Telemetry_Body)
+Telemetry_Raw = read.csv('.//LOGS/OpenHardwareMonitorLog-2023-04-23.csv', header=T)
+Telemetry_Plot = Telemetry_Raw[-(1:1),]
 
-plot(Telemetry$CPU.Total, Telemetry$CPU.Package)
+
+plot(Telemetry_Plot)
+
+
+
+
+
